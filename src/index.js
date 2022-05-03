f3acde158096c5b669cb8ffa4d1ba344c24003fd
trekitch <trekitch@ymail.com>
2022-05-03 15:57:35 -0400

    added more minor configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import "./style.css";
+import Restaurant from "./restaurant.jpg";
+
+function homePage() {
+    const content = document.querySelector("#content");
+    const header = document.createElement("h1");
+    const aboutText = document.createElement("p");
+
+    const restaurantPic = new Image();
+    restaurantPic.src = Restaurant;
+
+    content.appendChild(restaurantPic);
+
+    return content;
+}
+
+document.body.appendChild(homePage());
