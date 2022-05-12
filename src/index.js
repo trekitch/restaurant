766df1c58eb092f8642b869bd6bc844ab157b90e
trekitch <trekitch@ymail.com>
2022-05-12 03:52:21 -0400

    adjusted index and main js files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
+import "./cssreset.css";
 import "./style.css";
 import bbqPic from "./ribs.jpg";
 
 function homePage() {
     const content = document.querySelector("#content");
-    const header = document.createElement("h1");
+    const header = document.createElement("p");
     const aboutText = document.createElement("p");
-
-    header.textContent = "This is a header";
-    content.appendChild(header);
 
     const bbqImg = new Image();
     bbqImg.src = bbqPic;
+    bbqImg.classList.add("center");
+
+    header.textContent = "Welcome to Joe BBQ Shack";
 
     content.appendChild(bbqImg);
+    content.appendChild(header);
 
     return content;
 }
